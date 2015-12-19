5da90c5c890c2f20f4f8952c74b42e5b5bd5c915
Kirk Mayo <kirk.mayo@solnet.co.nz>
2015-12-18 16:38:21 -0800

    Adding more testing

--- tests/SiteMapControllerTest.php
+++ tests/SiteMapControllerTest.php
@@ -10,10 +10,35 @@ class  SiteMapControllerTest extends FunctionalTest {
 
     private $sitemap = 'sitemap-test.xml';
 
+    public function setUp() {
+        parent::setUp();
+        for ($i=0; $i<100; $i++) {
+            $page = new Page(
+                array(
+                    'title' => "Page $i"
+                )
+            );
+            $page->write();
+            $page->publish('Stage', 'Live');
+        }
+        $sitemap = new SiteMapPage();
+        $sitemap->Title = 'SiteMap';
+        $sitemap->write();
+    }
+
     public function tearDown() {
         parent::tearDown();
         if (file_exists(ASSETS_PATH . "/{$this->sitemap}")) {
             unlink(ASSETS_PATH . "/{$this->sitemap}");
+        }
+
+        $pages = Page::get();
+        foreach ($pages as $page) {
+            $page->delete();
+        }
+        $pages = SiteMapPage::get();
+        foreach ($pages as $page) {
+            $page->delete();
         }
     }
 
@@ -22,6 +47,24 @@ class  SiteMapControllerTest extends FunctionalTest {
         $controller = new SiteMapXMLController();
         $controller->generateSiteMap($sitemap);
 
+        $contents = file_get_contents($sitemap);
+        $found = strpos($contents, 'urlset');
+        $this->assertnotNull($found);
+
+        $xml = new SimpleXMLElement($contents);
+        $this->assertEquals(count($xml->url), 101);
+
         $this->assertTrue(file_exists($sitemap));
+    }
+
+    public function testHTMLGeneration() {
+        $controller = new SiteMapPage_Controller();
+        $html = $controller->getHierarchicalSitemapHTML();
+
+        $found = strpos($html, '<div>');
+        $this->assertnotNull($found);
+
+        $found = strpos($html, '<li>');
+        $this->assertnotNull($found);
     }
 }
