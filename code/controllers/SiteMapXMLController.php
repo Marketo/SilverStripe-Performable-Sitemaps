17582ae4f2108b837af76ffab231fcbd4c517eca
Marcus Nyeholt <marcus@silverstripe.com.au>
2016-02-25 08:01:38 +1100

    FIX Allow differing hostnames
    Allow detection of different domains prior to generating the sitemap.xml file
    to handle regional sites

--- code/controllers/SiteMapXMLController.php
+++ code/controllers/SiteMapXMLController.php
@@ -12,28 +12,42 @@ class SiteMapXMLController extends Controller
     );
 	
 	private static $base_url = '';
+    
+    private static $allowed_sub_hosts = array();
+    
+    private static $sitemap_name = 'sitemap.xml';
+    
+    private static $protocol = 'https';
 
     public function index(SS_HTTPRequest $request)
     {
-        $url = $request->getVar('url');
-        $sitemap = ASSETS_PATH . $url;
+        $sitemap = ASSETS_PATH . '/' . $this->config()->sitemap_name;
+        $subhosts = $this->config()->allowed_sub_hosts;
+        
+        $generateAsHost = null;
+        
+        if (is_array($subhosts) && in_array($_SERVER['HTTP_HOST'], $subhosts)) {
+            $generateAsHost = $_SERVER['HTTP_HOST'];
+            $sitemap = ASSETS_PATH .'/'. $generateAsHost . '.' . $this->config()->sitemap_name;
+        }
+        
         if (file_exists($sitemap) && !isset($_GET['flush'])) {
             $now = time();
             $fileAge = filemtime($sitemap);
             $age = ($now - $fileAge);
             if ($age > 86400) {
                 unlink($sitemap);
-                $this->generateSiteMap($sitemap);
+                $this->generateSiteMap($sitemap, $generateAsHost);
             }
         } else {
-            $this->generateSiteMap($sitemap);
+            $this->generateSiteMap($sitemap, $generateAsHost);
         }
         header("Content-Type:text/xml");
         readfile($sitemap);
         exit;
     }
 
-    public function generateSiteMap($sitemap = 'sitemap.xml')
+    public function generateSiteMap($sitemap = 'sitemap.xml', $siteURL = null)
     {
         $siteData = singleton('SiteDataService');
         $pages = $siteData->getItems();
@@ -46,10 +60,18 @@ class SiteMapXMLController extends Controller
             'http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd'
         );
 		
-		$siteURL = self::config()->get('base_url');
-		if (!$siteURL) {
-			$siteURL = Director::absoluteBaseURL();
-		}
+        if (!$siteURL) {
+            $siteURL = self::config()->get('base_url');
+            if (!$siteURL) {
+                $siteURL = Director::absoluteBaseURL();
+            }
+        }
+        
+        $siteURL = rtrim($siteURL, '/') . '/';
+        
+        if (!strpos($siteURL, ':/')) {
+            $siteURL = $this->config()->protocol . '://' . $siteURL;
+        }
         
         foreach ($pages as $page) {
             $url = $xml->addChild('url');
